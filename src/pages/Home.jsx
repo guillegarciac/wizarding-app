806546aa67c3eb3e59fa2c90cf1a7156a886da3c
guille garcia <ggarciacardiel@gmail.com>
2023-03-11 11:45:14 +0100

    it2 - home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [houses, setHouses] = useState([]);
+
+  const getHouses = async () => {
+    try {
+      const response = await axios.get('https://wizard-world-api.herokuapp.com/houses');
+      setHouses(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getHouses()
+  }, [])
+
   return (
-    <div>Hogwarts houses</div>
+    <div>
+      <h1>Hogwarts houses</h1>
+      {houses.map(elem => {
+        return (
+          <div key={elem.id}>
+            <h2>{elem.name}</h2>
+            <p>{elem.founder}</p>
+            <Link to={`/houses/${elem.id}`}>See House</Link>
+
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
